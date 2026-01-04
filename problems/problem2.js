a3a2d9f2061aed6a43e6c9634e7e5842d9dacfc2
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-04 00:36:17 -0600

    docs: add review comment to problem 2

--- problems/problem2.js
+++ problems/problem2.js
@@ -4,6 +4,8 @@ It takes one argument, an array of numbers
 
 
 It returns how many numbers in the array are positive (greater than 0) */
+// Reviewed solution and confirmed logic works correctly
+
 
 function countPositives(arr) {
   // Fail safely if input is not an array
