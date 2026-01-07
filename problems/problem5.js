739f7cf476b39605551095a5b6585d5bb47bc3ee
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-07 09:27:50 -0600

    docs: add problem 5 description and requirements

--- problems/problem5.js
+++ problems/problem5.js
@@ -1,3 +1,13 @@
+/* Problem statement: Given an array, write a Javascript function that returns an object that has the structure:
+{
+  positiveCount: integer,
+  negativeSum: integer
+}
+where the positiveCount is the count of positive numbers and the negativeSum is the sum of negative numbers.
+
+// Ex: [1, 2, -1, -2] -> {positiveCount: 2, negativeSum: -3}
+*/
+
 const countPositivesSumNegatives = (array) => {
   let positiveCount = 0;
   let negativeSum = 0;
