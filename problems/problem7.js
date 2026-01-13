f2d5441264c985eaad8a181b106acf1365a107a2
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-13 17:04:10 -0600

    Handle non-number values in sum function

--- problems/problem7.js
+++ problems/problem7.js
@@ -19,8 +19,10 @@ function sum(numbers) {
   let total = 0;
 
   for (let i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] !== 'number') {
     total += numbers[i];
   }
+}
 
   return total;
 }
