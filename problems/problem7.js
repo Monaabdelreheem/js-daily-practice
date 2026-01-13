00f7762bdc649018f628e7ac45e88427689ef01b
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-13 17:00:22 -0600

    Add comments for  problem 7 description and requirements

--- problems/problem7.js
+++ problems/problem7.js
@@ -1,3 +1,16 @@
+/*Write a function called sum
+
+Requirements:
+
+It takes one parameter, an array of numbers
+
+It returns the sum of all numbers in the array
+
+If the array is empty, return 0
+
+Do not use built-in methods like reduce */
+
+
 function sum(numbers) {
   let total = 0;
 
