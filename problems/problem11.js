0b8bd1339cef26374cc282868512758c7d6ec501
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-20 18:44:15 -0600

    docs: add detailed comments to problem 11 roman numeral logic

--- problems/problem11.js
+++ problems/problem11.js
@@ -5,6 +5,7 @@ Task:
 - No need to validate the Roman numeral format
 */
 
+// decimal values of Roman numerals
 function romanToInt(roman) {
   const values = {
     I: 1,
@@ -16,18 +17,25 @@ function romanToInt(roman) {
     M: 1000,
   };
 
+  // Initialize total to 0
   let total = 0;
 
+  // Iterate through each character in the Roman numeral string
   for (let i = 0; i < roman.length; i++) {
+    // Get the decimal value of the current and next Roman numeral
     const current = values[roman[i]];
+    // Get the decimal value of the next Roman numeral, if it exists
     const next = values[roman[i + 1]];
 
+    // If the current numeral is less than the next, subtract it from total
+    // Otherwise, add it to total
     if (next && current < next) {
       total -= current;
     } else {
       total += current;
     }
   }
-
+  
+// Return the final decimal value
   return total;
 }
