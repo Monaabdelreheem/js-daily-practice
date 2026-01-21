5a0cbd7077df27f66076d2e55620904cd96ed56d
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-21 14:00:00 -0600

    docs: add problem description for problem 12

---
+++ problems/problem12.js
@@ -0,0 +1,8 @@
+/* Problem 12: Break Camel Casing
+
+Task:
+- Take a camelCase string as input
+- Insert a space before each uppercase letter
+- Return the updated string
+- If the string is empty, return an empty string
+*/
