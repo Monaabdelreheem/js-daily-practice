70102c36946ff4e2535d641f206c85444cd00020
Mona Abdelreheem <m.abdelraheem@aol.com>
2026-01-21 18:00:00 -0600

    feat: complete problem 12 solution with task requirements

--- problems/problem12.js
+++ problems/problem12.js
@@ -3,9 +3,12 @@
 Task:
 - Take a camelCase string as input
 - Insert a space before each uppercase letter
+- Do not modify lowercase letters
+- Preserve the original order of characters
 - Return the updated string
 - If the string is empty, return an empty string
 */
+
 
 function solution(string) {
   return string.replace(/([A-Z])/g, " $1");
